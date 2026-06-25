ae16583b66b4201ba8e231318ff9da5f0a5d11ad
Shreyas Viswanathan <shreyasviswanathan1@gmail.com>
2026-06-24 20:58:27 -0500

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # s1 can be a permutation of itself so if its a substring of s2 return right away
+        if s1 in s2:
+            return True
+        
+        n, m = len(s1), len(s2)
+        left = 0
+
+        sorted_s1 = ''.join(sorted(s1))
+
+        for right in range(n - 1, m):
+            curr_window = s2[left:right+1]
+            chars = ''.join(sorted(curr_window))
+
+            if chars == sorted_s1:
+                return True
+            
+            left += 1
+        
+        return False
