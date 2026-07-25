7c35c9518a78eb1b75fb7a60fcb97a0836b5636a
Shreyas Viswanathan <shreyasviswanathan1@gmail.com>
2026-07-25 14:48:16 -0500

    Add: valid-binary-search-tree - submission-13

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-13.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def bfs(self, root: Optional[TreeNode]) -> list[int]:
+        if not root:
+            return []
+        
+        q = deque([root])
+        res = []
+
+        while q:
+            node = q.popleft()
+            res.append(node.val)
+
+            if node.left:
+                q.append(node.left)
+            
+            if node.right:
+                q.append(node.right)
+        
+        return res
+        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        l_elems = self.bfs(root.left)
+        r_elems = self.bfs(root.right)
+
+        print(f"root that we branched of of: {root.val}")
+        print(f"l_elems: {l_elems}")
+        print(f"r_elems: {r_elems}")
+        
+        if not all(elem < root.val for elem in l_elems) or not all(elem > root.val for elem in r_elems):
+            return False
+        
+        # everything on left and right is fine now we needa check subtrees also
+        return self.isValidBST(root.left) and self.isValidBST(root.right)
